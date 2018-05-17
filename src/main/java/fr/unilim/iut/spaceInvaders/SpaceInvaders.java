961a72fc1cf995b8e0d8201071663912b8df5b23
Nathan <nathan.mousset@etu.unilim.fr>
2018-05-18 00:51:03 +0200

    Story Déplacer le vaisseau vers droite et gauche ( fin fonctionalité 1)

--- src/main/java/fr/unilim/iut/spaceInvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceInvaders/SpaceInvaders.java
@@ -16,6 +16,9 @@ public class SpaceInvaders {
    }
     @Override
     public String toString() {
+		return recupererEspaceJeuDansChaineASCII();
+	}
+	String recupererEspaceJeuDansChaineASCII() {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int y = 0; y < hauteur; y++) {
 			for (int x = 0; x < longueur; x++) {
@@ -51,4 +54,12 @@ public void positionnerUnNouveauVaisseau(int x, int y) {
 private boolean estDansEspaceJeu(int x, int y) {
 	return !(((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur)));
 }
+public void deplacerVaisseauVersLaDroite() {
+	if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+}
+public void deplacerVaisseauVersLaGauche() {
+	if (vaisseau.abscisse()< 0) vaisseau.seDeplacerVersLaGauche();
+	
+}
+
 }
